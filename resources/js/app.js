f0a1e529f4181792fe90c6426de81133cd211632
christiansaccani <christian.saccani99@gmail.com>
2024-04-15 07:55:01 +0200

    exercise done with bonus

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 
 // Import our custom CSS
 import '~resources/scss/app.scss'
+import '~bootstrap/js/index.esm.js'
 import * as bootstrap from 'bootstrap'
 
 //precompilazione immagini
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -53,9 +53,35 @@
         </div>
     </div>
         
-
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Elimina
+    </button>
     
     
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminazione</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Sei sicuro di voler elimare il file: {{$comic->title}}?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+  
+          <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger">Elimina</button>
+          </form>
+  
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection

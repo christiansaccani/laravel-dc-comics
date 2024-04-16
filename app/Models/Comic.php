abaa8d0b87dee3ea7a5531b63d979407fd528b3a
christiansaccani <christian.saccani99@gmail.com>
2024-04-16 18:51:21 +0200

    exercise done!

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
+
+    // laravel fornisce degli strumenti e proprietà che permettono di gestire la soft delete,
+    // quindi dobbiamo solo farla ereditare con i traits
+    use SoftDeletes;
 }
